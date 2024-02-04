c938db380ab4cbab547f15ee64fc70131d5615b9
DavidBusiness <davidhagebusiness@gmail.com>
2024-02-04 19:11:08 +0200

    Added category options and refactored code

--- src/app/models/EstablishmentDataModel.ts
+++ src/app/models/EstablishmentDataModel.ts
@@ -1,78 +1,26 @@
 export interface EstablishmentDataModel {
-  establishment: Establishment;
+  establishmentName: string;
   categories: CategoryDataModel[];
 }
 
 export interface CategoryDataModel {
-  category: Category;
-  menuItems: MenuItem[];
-  categoryOptions: CategoryOptions[];
-}
-
-export interface Establishment {
   id: number;
   name: string;
-  address: string;
-  phone: string;
-  website: string;
-  description?: string;
-  imageUrl?: string;
-  dateAdded: Date;
-  categories?: Category[];
+  menuItems: MenuItemDataModel[];
+  categoryOptions: CategoryOptionsDataModel[];
 }
 
-export interface Category {
-  id: number;
-  name: string;
-  isEnabled: boolean;
-  establishmentId: number;
-  establishment?: Establishment;
-  menuItems?: MenuItem[];
-  recurrenceDays?: CategoryRecurrenceDay[];
-}
-
-export interface CategoryOptions {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-  establishmentId: number;
-  establishment?: Establishment;
-}
-
-export interface MenuItem {
+export interface MenuItemDataModel {
   id: number;
   name: string;
   description: string;
   price: number;
-  isEnabled: boolean;
-  categoryId: number;
-  category?: Category; // Assuming you have a Category interface
-  categoryOptions?: CategoryOptions[];
-  recurrenceDays?: MenuItemRecurrenceDay[];
-  itemIndicator?: number;
+  itemIndicator: number;
 }
 
-export interface MenuItemRecurrenceDay {
+export interface CategoryOptionsDataModel {
   id: number;
-  day: DayOfWeek;
-  menuItemId: number;
-  menuItem?: MenuItem; // Assuming you have a MenuItem interface
-}
-
-export interface CategoryRecurrenceDay {
-  id: number;
-  day: DayOfWeek;
-  categoryId: number;
-  category?: Category; // Assuming you have a Category interface
-}
-
-export enum DayOfWeek {
-  Sunday,
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
+  name: string;
+  price: number;
+  description: string;
 }
